900ab8cf1c738838f4fd7ac94c160c7cc5726b01
Connor Mullan <cmullan602@gmail.com>
2022-08-30 10:00:53 -0700

    added dotenv file.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,19 +6,24 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  //category.findAll()
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  //category.findByPk(req.params.id)
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  //category.create(req.body)
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+
+  //category.update(req.body, where)
 });
 
 router.delete('/:id', (req, res) => {
